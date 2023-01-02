1ccf8eac01e7610a5b2343012bc1c5724a735aba
nabiursiddique <nabiursiddique01@gmail.com>
2023-01-02 23:57:00 +0600

    Now we can get the data form the database and post the booking data into the database

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express()
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7t8vw3l.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const appointmentOptionCollection = client.db("doctorsPortal").collection("appointmentOptions");
+        const bookingsCollection = client.db("doctorsPortal").collection("bookings")
+
+        app.get('/appointmentOptions', async (req, res) => {
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            res.send(options);
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
+
 
 app.get('/', async (req, res) => {
     res.send('Doctors portal server is running');
